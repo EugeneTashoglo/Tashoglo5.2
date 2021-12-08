ed6b6c885029180a8d19e42aa3fa2e1dbba8bf28
Евгений Ташогло <tashoglo00@mail.ru>
2021-12-08 18:52:43 +0200

    Добавьте файлы проекта.

---
+++ Mylib/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Mylib
+{
+    public class Class1
+    {
+       public static int[] SumNegative(int[,] mas)
+        {
+            {
+                int rows = mas.GetUpperBound(0) + 1;
+                int columns = mas.Length / rows;
+                int[] res = new int[rows];
+                for (int i = 0; i < rows; i++)
+                {
+                    int S = 0;
+                    for (int j = 0; j < columns; j++)
+                    {
+                        if (mas[i, j] < 0) S += mas[i, j];
+                    }
+                    res[i] = S;
+                }
+                return res;
+            }
+        }
+    }
+}
---
+++ Tashoglo5.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Tashoglo5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mas = new int[8, 3];
+            Random rnd = new Random();
+            for(int i=0;i<8;i++)
+            {
+                for (int j=0;j<3;j++)
+                {
+                    mas[i, j] = rnd.Next(-10, 11);
+                    Console.Write(mas[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+            int[] res = Mylib.Class1.SumNegative(mas);
+            for (int i = 0 ; i < res.Length; i++)
+
+                {
+                {
+                    Console.Write(res[i] + "  ");
+
+                }
+                Console.WriteLine();
+            }
+                
+        }
+    }
+}
